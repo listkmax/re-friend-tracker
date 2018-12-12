6608df28fd934e0f26c123113fdaeb396a1c80df
listkmax <listkmax@students.zhaw.ch>
2018-12-12 14:54:48 +0100

    Update

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -6,7 +6,7 @@ export class GuiModel {
 
     private _guiModel = {
         "application": {
-            "title": "Requirements Engineering Friend Tracker",
+            "title": "Maxim Friend Tracker",
             "formList": [
                 {
                     "id": "FriendForm",
